6898481167b633dfe23736819570ba0a6550574f
misua <charlespogzz@googlemail.com>
2022-06-07 17:02:43 +0200

    try pg db in template

--- models/setup.go
+++ models/setup.go
@@ -2,13 +2,19 @@ package models
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	// "gorm.io/driver/postgres"
+	// "gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	database, err := gorm.Open("sqlite3", "test.db")
+
+	database, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=postgres password=postgres")
+	defer database.Close()
+
+	//database, err := gorm.Open("postgres", dsn.String())
 
 	if err != nil {
 		panic("failed to connect to db")
@@ -17,3 +23,23 @@ func ConnectDatabase() {
 
 	DB = database
 }
+
+// package models
+
+// import (
+// 	"github.com/jinzhu/gorm"
+// 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+// )
+
+// var DB *gorm.DB
+
+// func ConnectDatabase() {
+// 	database, err := gorm.Open("sqlite3", "test.db")
+
+// 	if err != nil {
+// 		panic("failed to connect to db")
+// 	}
+// 	database.AutoMigrate(&Book{})
+
+// 	DB = database
+// }
