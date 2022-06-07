d7af0644e8b0649e90cec8f4f18ec5c5a74cf649
misua <charlespogzz@googlemail.com>
2022-06-07 17:19:20 +0200

    adding docker

--- models/setup.go
+++ models/setup.go
@@ -24,6 +24,7 @@ func ConnectDatabase() {
 	DB = database
 }
 
+// docker run --name basic-postgres --rm -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=postgres -e PGDATA=/var/lib/postgresql/data/pgdata -v /tmp:/var/lib/postgresql/data -p 5432:5432 -it postgres:14.1-alpine
 // package models
 
 // import (
